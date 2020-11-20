dd737a00604ad7907079a2594ea067b41c57058f
huyudong <379145942@qq.com>
2020-11-20 10:02:11 +0800

    'rule'

--- glposition/src/share/huInput.vue
+++ glposition/src/share/huInput.vue
@@ -2,13 +2,13 @@
     <div>
       <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
         <el-form-item :label="label" prop="email" v-if="type=='email'">
-          <el-input v-model="ruleForm.email" placeholder="请输入你的邮箱" clearable></el-input>
+          <el-input v-model="ruleForm.email" placeholder="请输入你的邮箱" clearable style="ime-mode:disabled" maxlength='30'></el-input>
         </el-form-item>
         <el-form-item :label="label" prop="name" v-if="type=='name'">
-          <el-input v-model="ruleForm.name" placeholder="请输入你的用户名" clearable></el-input>
+          <el-input v-model="ruleForm.name" placeholder="请输入你的用户名" clearable onkeyup="value=value.replace(/[^\w\.\/]/ig,'')" maxlength='20'></el-input>
         </el-form-item>
         <el-form-item :label="label" prop="phone" v-if="type=='phone'">
-          <el-input v-model="ruleForm.phone" placeholder="请输入你的手机号码" clearable></el-input>
+          <el-input v-model="ruleForm.phone" placeholder="请输入你的手机号码" clearable onkeyup="this.value=this.value.replace(/\D/g,'')" maxlength='11'></el-input>
         </el-form-item>
         <el-form-item :label="label" prop="code" v-if="type=='code'">
           <el-input v-model="ruleForm.code" placeholder="请输入验证码" clearable style="width: 200px"></el-input>
@@ -24,20 +24,6 @@
         name: "huInput",
         props:['label','required','type'],
         data(){
-          var checkPhone=(rule, value, callback)=>{
-            if (!value) {
-              return callback(new Error('手机号码不能为空'));
-            }
-            if (!this.$isNumber(value)) {
-              callback(new Error('请输入数字值'));
-            } else {
-              if (value.length != 11) {
-                callback(new Error('电话格式不正确'));
-              } else {
-                callback();
-              }
-            }
-          }
           var checkCode=(rule, value, callback)=>{
             if (!value) {
               return callback(new Error('验证码不能为空'));
@@ -60,14 +46,13 @@
             rules:{
               email:[
                 { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-                { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur'] }
+                { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' },
               ],
               name: [
                 { required: true, message: '请输入名称', trigger: 'blur' },
-                { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
               ],
               phone:[
-                { required:true,validator: checkPhone,trigger: 'blur'}
+                { required:true,message: '请输入手机号码',trigger: 'blur'}
               ],
               code:[
                 { required:true,validator: checkCode,trigger: 'blur'}
