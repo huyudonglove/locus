1c1d117ddca4de12a2f303f5d056d108351f3587
huyudong <379145942@qq.com>
2020-11-23 15:00:15 +0800

    'login'

--- glposition/src/components/log/forgot.vue
+++ glposition/src/components/log/forgot.vue
@@ -8,10 +8,8 @@
           <img src="../../assets/dui.png" alt="" width="66px" style="display: inline-block;vertical-align:middle">
           <img src="../../assets/User.png" alt="" style="margin-left: 50px;;display: inline-block;vertical-align:middle">
         </div>
-        <!--        <div style="padding-left: 20px">-->
-        <!--          重置密码邮件已经发送到您的邮箱（{{this.$refs.email.ruleForm.email}}）中。-->
-        <!--          请去邮箱完成注册流程。-->
-        <!--        </div>-->
+        <p style="max-height: 20px">验证邮件已经发送到您的邮箱（{{this.$refs.email.ruleForm.email}}）中。</p>
+        请去邮箱完成重置流程。
         <div style="text-align: center">
           <el-button type="primary" @click="change" style="width: 200px">返回登录</el-button>
         </div>
